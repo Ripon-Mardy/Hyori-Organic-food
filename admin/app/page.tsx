365412ef971ee729e35053ed083a4e6249dfa815
Ripon-Mardy <mardyripon10@gmail.com>
2026-03-24 17:59:14 +0600

    Add frontend and admin properly

---
+++ admin/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-5xl uppercase font-bold text-center text-center">
+        admin Dashboard
+      </h2>
+    </div>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-5xl font-bold text-center uppercase">Hoyri</h2>
+    </div>
+  );
+}
