d3b49ed745d11c90f021544a64dcc46ae3bdaad8
Ripon-Mardy <mardyripon10@gmail.com>
2026-04-01 13:24:40 +0600

    add

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h2 className="text-5xl uppercase font-bold text-center text-center">
+      <h2 className="text-5xl uppercase font-bold text-center">
         admin Dashboard
       </h2>
     </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import path from "path";
 
 export default function Home() {
   return (
