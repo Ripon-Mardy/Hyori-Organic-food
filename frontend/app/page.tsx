b3ab5236b90cd923c4004b6297c604177b8bc96a
Ripon-Mardy <mardyripon10@gmail.com>
2026-05-18 19:50:01 +0600

    add best sellet section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import BannerBottom from "@/components/BannerBottom";
 import BannerSlider from "@/components/BannerSlider";
+import BestSeller from "@/components/BestSeller";
 
 export default function Home() {
   return (
     <>
       <BannerSlider />
       <BannerBottom />
+      <BestSeller />
     </>
   );
 }
--- frontend/components/BestSeller.tsx
+++ frontend/components/BestSeller.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import Image from "next/image";
+
+// --------- best seller data ------------
+const bestSellerData = [
+  {
+    id: 1,
+    name: "Banana",
+    image: "/products/banana.png",
+    price: 169,
+    oldPrice: null,
+    rating: 4,
+    reviews: 1,
+    category: "Fruits",
+  },
+  {
+    id: 2,
+    name: "Fresh Meat",
+    image: "/products/fresh-meat.png",
+    price: 158,
+    oldPrice: null,
+    rating: 4,
+    reviews: 1,
+    category: "Meat",
+  },
+  {
+    id: 3,
+    name: "Orange",
+    image: "/products/orange.png",
+    price: 120,
+    oldPrice: null,
+    rating: 3,
+    reviews: 1,
+    category: "Fruits",
+  },
+  {
+    id: 4,
+    name: "Delicious Meat",
+    image: "/products/delicious-meat.png",
+    price: 150,
+    oldPrice: null,
+    rating: 0,
+    reviews: 0,
+    category: "Meat",
+  },
+  {
+    id: 5,
+    name: "Carrot",
+    image: "/products/carrot.png",
+    price: 188,
+    oldPrice: null,
+    rating: 0,
+    reviews: 0,
+    category: "Vegetables",
+  },
+  {
+    id: 6,
+    name: "Red Apple",
+    image: "/products/red-apple.png",
+    price: 155,
+    oldPrice: 167,
+    rating: 3,
+    reviews: 1,
+    category: "Fruits",
+  },
+];
 
 const BestSeller = () => {
-  return <div>BestSeller</div>;
+  return (
+    <section className="py-16 best-seller-bg">
+      <div className="max-w-(--container-width) mx-auto px-2">
+        {/* -- left side image -- */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default BestSeller;
